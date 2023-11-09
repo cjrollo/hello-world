78fdde6cd0c4c957b34f32c1eee1ddeb5004c7a5
rollo <rollo.nike@gmail.com>
2023-11-08 22:12:56 -0800

    added clarifying comment

--- hello-world_test.go
+++ hello-world_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 
+// Describes a simple cycle of red-green-refactor
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
